7fa3fd45962785540afff602ca7fd4a81f08f04c
ilapygin <igor.lapygin@gmail.com>
2024-06-24 11:18:08 +0300

    HW02_function

---
+++ Python Core New Hw02_9.py
@@ -0,0 +1,45 @@
+'''
+Необхідно створити функцію discount_price на Python, яка розраховує кінцеву ціну товару після застосування знижки.
+
+Задачі:
+
+Створіть функцію discount_price, яка приймає два аргументи: price - початкова ціна товару та discount - знижка як 
+дійсне число від 0 до 1.
+Усередині функції discount_price створіть вкладену функцію apply_discount, яка використовує nonlocal для доступу 
+та модифікації змінної price.
+Функція apply_discount має обчислити знижену ціну, помноживши price на (1 - discount).
+Викличте apply_discount всередині discount_price, а потім поверніть оновлену ціну.
+Очікуваний результат:
+
+Функція повинна повертати ціну товару після застосування знижки.
+
+Підказки:
+
+Використання nonlocal дозволяє функції apply_discount модифікувати змінну price, оголошену у зовнішній функції discount_price.
+Для розрахунку зниженої ціни використовуйте формулу price * (1 - discount)
+
+'''
+def discount_price(price, discount):
+    """
+    Розраховує кінцеву ціну товару після застосування знижки.
+
+    Parameters:
+    price (float): Початкова ціна товару.
+    discount (float): Знижка як число від 0 до 1.
+
+    Returns:
+    float: Кінцева ціна товару після застосування знижки.
+    """
+    def apply_discount():
+        nonlocal price
+        price = price * (1 - discount)
+    
+    apply_discount()
+    return price
+
+# Приклад використання функції
+initial_price = 100.0  # Початкова ціна товару
+discount_rate = 0.1  # Знижка як число від 0 до 1
+
+final_price = discount_price(initial_price, discount_rate)
+print(f"Кінцева ціна після знижки: {final_price}")
---
+++ test.py
@@ -0,0 +1,5 @@
+def is_even(num: int):
+    return num % 2 == 0
+
+check_even = is_even(4)
+print(check_even)  # Виведе: True
